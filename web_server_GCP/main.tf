9a6e804770f6438d8b8ea64e236d68811124c452
aleksandrandreichenko <andreichenko@web.de>
2021-01-15 20:58:56 +0300

    terraform for GCP

--- web_server_GCP/main.tf
+++ web_server_GCP/main.tf
@@ -6,3 +6,18 @@ provider "google" {
   zone = "us-west1-c"
 }
 
+resource "google_compute_instance" "my_server" {
+  name = "my-gcp-server"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"  // this enable private ip address
+    access_config {}  // this enable public ip address
+  }
+}
+
