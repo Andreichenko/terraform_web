81748ec0de52e02c4de7d3bfd2bd37db75a9c99d
aleksandrandreichenko <andreichenko@web.de>
2021-01-26 17:00:26 +0300

    Use Variables and Multiply files

---
+++ using_variables/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
---
+++ using_variables/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "Region where you want to provision this EC2 WebServer"
+  type        = string // number , bool
+  default     = "ca-central-1"
+}
