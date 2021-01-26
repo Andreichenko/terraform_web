93aa8ddd5aae606694e0ea63f0865e54d13990d1
aleksandrandreichenko <andreichenko@web.de>
2021-01-26 17:03:24 +0300

    Use Variables and Multiply files

--- using_variables/variables.tf
+++ using_variables/variables.tf
@@ -15,3 +15,30 @@ variable "instance_size" {
   type        = string
   default     = "t3.micro"
 }
+
+variable "tags" {
+  description = "Tags to Apply to Resources"
+  type        = map(any)
+  default = {
+    Owner       = "Frei"
+    Environment = "Prod"
+    Project     = "Phoenix"
+  }
+}
+
+variable "key_pair" {
+  description = "SSH Key pair name to ingest into EC2"
+  type        = string
+  default     = "KeyValue"
+  sensitive   = true
+}
+
+variable "password" {
+  description = "Please Enter Password lenght of 10 characters!"
+  type        = string
+  sensitive   = true
+  validation {
+    condition     = length(var.password) == 10
+    error_message = "Your Password must be 10 characted exactly!!!"
+  }
+}
