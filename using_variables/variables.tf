69cfa99b9c42dab45f1adf34b91c9b1ec3bd6530
aleksandrandreichenko <andreichenko@web.de>
2021-01-26 17:01:08 +0300

    Use Variables and Multiply files

--- using_variables/variables.tf
+++ using_variables/variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string // number , bool
   default     = "ca-central-1"
 }
+
+variable "port_list" {
+  description = "List of Poret to open for our WebServer"
+  type        = list(any)
+  default     = ["80", "443"]
+}
+
+variable "instance_size" {
+  description = "EC2 Instance Size to provision"
+  type        = string
+  default     = "t3.micro"
+}
