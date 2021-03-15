493b44dbbe8ead438faac4781e2adbbab769a330
aleksandrandreichenko <andreichenko@web.de>
2021-03-15 03:01:58 +0300

    Use Variables and Multiply files

---
+++ variables_in_project/variables.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  description = "Region where you want to provision this EC2 WebServer"
+  type        = string // number , bool that's ok
+  default     = "eu-central-1"
+}
+
+variable "port_list" {
+  description = "List of port to open for our WebServer"
+  type        = list(any)
+  default     = ["80", "443"]
+}
+
+variable "instance_size" {
+  description = "EC2 Instance Size to provision"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "tags" {
+  description = "Tags to Apply to Resources"
+  type        = map(any)
+  default = {
+    Owner       = "Sasha Frei"
+    Environment = "Prod"
+    Project     = "Sync"
+  }
+}
+
+variable "key_pair" {
+  description = "SSH Key pair name to ingest into EC2"
+  type        = string
+  default     = "Frei_default"
+  sensitive   = true
+}
+
+variable "password" {
+  description = "Please Enter Password lenght of 10 characters!"
+  type        = string
+  sensitive   = true
+  validation {
+    condition     = length(var.password) == 10
+    error_message = "Your Password must be 10 charact exactly!!!"
+  }
+}
+
